d5b082d3f619d48059ebfdafc7732499f55d2b3e
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-01-25 19:10:32 -0800

    dev(category) : json added

--- app/(store)/[categoryId]/page.tsx
+++ app/(store)/[categoryId]/page.tsx
@@ -1,8 +1,14 @@
-type Params = Promise<{categoryId: string}>
+type Params = Promise<{categoryId: string}>;
 
+async function getProducts(categoryId: string) {
+    const url = `${process.env.API_URL}/categories/${categoryId}?products=true`
+    const req = await fetch(url)
+    const json = await req.json()
+
+return json}
 export default async function StorePage({params}: { params: Params}) {
     const {categoryId} = await params
-    console.log("Category: ",categoryId)
+    const products= await getProducts(categoryId)
     return(
         <div>Page</div>
     )
