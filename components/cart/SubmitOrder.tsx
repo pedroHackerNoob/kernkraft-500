384be95b232343215faf154c369519dbea675997
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-01-31 10:47:33 -0800

    dev(Submit order) :  fix parse order

--- components/cart/SubmitOrder.tsx
+++ components/cart/SubmitOrder.tsx
@@ -1,7 +1,25 @@
+import {useActionState} from "react";
+import {submitOrderAction} from "@/actions/submit-order-action";
+import {useStore} from "@/src/store";
+
 export default function SubmitOrder() {
+    const coupon = useStore(state=> state.coupon.coupon?.name)
+    const contents = useStore(state=> state.contents)
+    const order= {coupon,contents}
+    console.log('SubmitOrder:\n',order)
+    const submitOrderWithData = submitOrderAction.bind(null,order)
+    const [state, dispatch]= useActionState(submitOrderWithData,{
+        errors:[],
+        succes: '',
+    })
     return(
-        <form>
-            <input type="submit" value="confirmar compra" className=" mt-5 w-full p-3 bg-indigo-600 hover:bg-indigo-700 text-white uppercase font-bold hover:cursor-pointer"/>
+        <form action={dispatch}>
+
+            <input type="submit"
+                   value="confirmar compra"
+                   className=" mt-5 w-full p-3 bg-indigo-600 hover:bg-indigo-700 text-white uppercase font-bold hover:cursor-pointer"
+
+            />
         </form>
     )
 }
