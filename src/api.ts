76b23f1b1cc54b6a6c3aee56a72a89aeff6042b2
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-01 16:44:13 -0800

    dev(admin) : added safe

--- src/api.ts
+++ src/api.ts
@@ -3,8 +3,8 @@ import {TransactionsResponseSchema} from "@/src/schema";
 export async function getSalesByDate(date: string){
     const url = `${process.env.NEXT_PUBLIC_DOMAIN}/admin/sales/api?transactionDate=${date}`;
     const req = await fetch(url);
+    if (!req.ok) throw new Error('Error en la petición');
     const json = await req.json();
-    const transactions = TransactionsResponseSchema.parse(json);
-    console.log('api\n',transactions);
-    return transactions;
+    const transactions = TransactionsResponseSchema.safeParse(json);
+    return transactions.data;
 }
